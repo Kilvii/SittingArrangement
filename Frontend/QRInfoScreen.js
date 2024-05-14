e065d060b9cf79f095d38e61320dcaab253c0c5a
Kirill Ryabikin <98740566+Kilvii@users.noreply.github.com>
2024-05-14 23:50:25 +0300

    Frontend update

--- Frontend/QRInfoScreen.js
+++ Frontend/QRInfoScreen.js
@@ -11,70 +11,75 @@ const QRInfoScreen = () => {
 
     const splittedData = qrData.split('; ');
     const phoneNumber = splittedData[6];
-    const cleanedPhoneNumber = phoneNumber.replace(/[\s-]/g, '');
+    const cleanedPhoneNumber = "+" + phoneNumber.replace(/[\s-]/g, '');
     splittedData[6] = cleanedPhoneNumber;
+    splittedData[5] = parseInt(splittedData[5])
+    splittedData[10] = parseInt(splittedData[10])
+    splittedData[13] = parseInt(splittedData[13])
+    splittedData[14] = parseInt(splittedData[14])
 
-    console.log('\nSplitted Data:', splittedData);
-
-    // axios.post('http://localhost:3000/api/placements/store', {
-    // "room_id": 1488,
-    // "number_of_seats": 25,
-    // "available_seats": 25
-    // }).then(response => {
-    //     console.log(response.data);
-    // }).catch(error => {
-    //     console.error(error);
-    // });
+    const [apiResponse, setApiResponse] = useState({});
 
     const makeAPICall = async () => {
         try {
-            fetch('https://jsonplaceholder.typicode.com/todos/1')
-                .then(response => response.json())
-                .then(json => console.log(json))
-            // const settings = {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     },
-            //     body: JSON.stringify({
-            //         "room_id": 1488,
-            //         "number_of_seats": 25,
-            //         "available_seats": 25
-            //     }),
-            // }
-            // const response = await fetch('http://localhost:3000/api/placements/store', settings);
-            // const data = await response.json();
-            // console.log({ response });
-            // console.log({ data });
+            const settings = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "surname": splittedData[0],
+                    "firstname": splittedData[1],
+                    "patronymic": splittedData[2],
+                    "gender": splittedData[3],
+                    "birthdate": splittedData[4],
+                    "age": splittedData[5],
+                    "phone": splittedData[6],
+                    "email": splittedData[7],
+                    "school": splittedData[8],
+                    "address": splittedData[9],
+                    "classroom": splittedData[10],
+                    "subject": splittedData[11],
+                    "citizenship": splittedData[12],
+                    "passport_series": splittedData[13],
+                    "passport_number": splittedData[14]
+                }),
+            }
+            const response = await fetch('http://5.42.220.6:3000/api/users/store', settings);
+            const json = await response.json();
+            return json;
         } catch (e) {
             console.log(e);
         }
     }
-    useEffect(() => {
-        makeAPICall();
-    }, [])
 
-    // const handleAddUser = async () => {
-    //     try {
-    //         const response = await axios.post('http://localhost:3000/api/placements/store', {
-    //             "room_id": 1488,
-    //             "number_of_seats": 25,
-    //             "available_seats": 25
-    //         });
-    //         console.log(response.data.message);
-    //     } catch (error) {
-    //         console.error('Error inserting user:', error.response.data.error);
-    //     }
-    // };
-    // handleAddUser()
+    useEffect(() => {
+        makeAPICall().then(response => {
+            setApiResponse(response);
+        });
+    }, [])
 
     return (
         <View style={styles.container}>
+          {apiResponse.user ? (
+            <>
+              <Text style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                {apiResponse.user.surname} {apiResponse.user.firstname} {apiResponse.user.patronymic}
+              </Text>
+              <Text style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                Садится в помещение №{apiResponse.user.room_id}
+              </Text>
+              <Text style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                На место под номером №{apiResponse.user.seat}
+              </Text>
+            </>
+          ) : (
             <Text style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                {qrData}
+              Loading...
             </Text>
+          )}
         </View>
-    );
+      );
 };
 
 const styles = StyleSheet.create({
