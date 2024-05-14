c7b06502ec2e2528512a4758fab25e4696aa0c68
Kirill Ryabikin <98740566+Kilvii@users.noreply.github.com>
2024-05-14 17:15:34 +0300

    Update users.js

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -2,178 +2,184 @@ var express = require('express');
 var router = express.Router();
 const pool = require('../db')
 
-function setPerson(newUser, placements, users) {
-  let currentRoom = null;
-  let currentRoomIndex = 0;
+function handleUserSeatingError(user) {
+  throw new Error(`Cannot seat user ${user.surname}`);
+}
+
+function setPerson(userToSeat, placements, users) {
+  let availableRoom = null;
+  let availableRoomIndex = 0;
   for (room of placements) {
     if (room['available_seats'] > 0) {
-      currentRoom = room;
+      availableRoom = room;
       break;
     }
-    currentRoomIndex += 1
+    availableRoomIndex += 1
   }
   //XXX: Если все помещения заняты
-  if (currentRoom === null) {
-    console.log('All rooms occupied')
-    pool.query('DELETE FROM users WHERE id = $1', [newUser['id']], (err, res) => {
+  if (availableRoom === null) {
+    // console.log('All rooms occupied')
+    pool.query('DELETE FROM users WHERE id = $1', [userToSeat['id']], (err, res) => {
       if (err) {
         console.log(err);
       } else {
-        console.log(`User with id ${newUser['id']} removed from database`);
+        console.log(`User with id ${userToSeat['id']} removed from database`);
       }
     });
+    handleUserSeatingError(userToSeat)
     return
   }
 
-  const filteredUsers = users.filter(user => user['room_id'] === currentRoom['room_id']);
+  const filteredUsers = users.filter(user => user['room_id'] === availableRoom['room_id']);
   const sortedUsers = filteredUsers.sort((a, b) => a['seat'] - b['seat']);
 
   //XXX: Если это первый добавляемый объект в помещение
   if (users.length == 1 || sortedUsers.length == 0) {
-    currentRoom['available_seats']--;
-    newUser['seat'] = 1;
-    newUser['room_id'] = currentRoom['room_id']
-    pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [currentRoom['available_seats'], currentRoom['room_id']], (err, res) => {
+    availableRoom['available_seats']--;
+    userToSeat['seat'] = 1;
+    userToSeat['room_id'] = availableRoom['room_id']
+    pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [availableRoom['available_seats'], availableRoom['room_id']], (err, res) => {
       if (err) {
         console.log(err);
         return;
       }
     });
-    pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [newUser['seat'], newUser['room_id'], newUser['id']], (err, res) => {
+    pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [userToSeat['seat'], userToSeat['room_id'], userToSeat['id']], (err, res) => {
       if (err) {
         console.log(err);
         return;
       }
     });
-    console.log(`User ${newUser['surname']} sits in room ${newUser['room_id']} in place ${newUser['seat']} :start: avail_seat = ${currentRoom['available_seats']} has left`);
+    console.log(`User ${userToSeat['surname']} sits in room ${userToSeat['room_id']} in place ${userToSeat['seat']} :start: avail_seat = ${availableRoom['available_seats']} has left`);
     return;
   }
 
   //XXX: Если люди из разных школ
-  if (newUser['school'] !== sortedUsers[sortedUsers.length - 1]['school']) {
+  if (userToSeat['school'] !== sortedUsers[sortedUsers.length - 1]['school']) {
     //XXX: Проверка на то, был ли скачок через место
     for (let i = 1; i < sortedUsers.length; i++) {
       if (sortedUsers[i]['seat'] - sortedUsers[i - 1]['seat'] !== 1) {
-        newUser['seat'] = (sortedUsers[i]['seat'] + sortedUsers[i - 1]['seat']) / 2;
-        newUser['room_id'] = currentRoom['room_id']
-        currentRoom['available_seats']--;
-        pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [currentRoom['available_seats'], currentRoom['room_id']], (err, res) => {
+        userToSeat['seat'] = (sortedUsers[i]['seat'] + sortedUsers[i - 1]['seat']) / 2;
+        userToSeat['room_id'] = availableRoom['room_id']
+        availableRoom['available_seats']--;
+        pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [availableRoom['available_seats'], availableRoom['room_id']], (err, res) => {
           if (err) {
             console.log(err);
             return;
           }
         });
-        pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [newUser['seat'], newUser['room_id'], newUser['id']], (err, res) => {
+        pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [userToSeat['seat'], userToSeat['room_id'], userToSeat['id']], (err, res) => {
           if (err) {
             console.log(err);
             return;
           }
         });
-        console.log(`User ${newUser['surname']} sits in room ${newUser['room_id']} in place ${newUser['seat']} :insert between: avail_seat = ${currentRoom['available_seats']} has left`);
+        console.log(`User ${userToSeat['surname']} sits in room ${userToSeat['room_id']} in place ${userToSeat['seat']} :insert between: avail_seat = ${availableRoom['available_seats']} has left`);
         return;
       }
     }
 
     //XXX: Посадить на следующее место
-    newUser['seat'] = sortedUsers[sortedUsers.length - 1]['seat'] + 1
-    newUser['room_id'] = currentRoom['room_id']
-    currentRoom['available_seats']--;
-    pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [currentRoom['available_seats'], currentRoom['room_id']], (err, res) => {
+    userToSeat['seat'] = sortedUsers[sortedUsers.length - 1]['seat'] + 1
+    userToSeat['room_id'] = availableRoom['room_id']
+    availableRoom['available_seats']--;
+    pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [availableRoom['available_seats'], availableRoom['room_id']], (err, res) => {
       if (err) {
         console.log(err);
         return;
       }
     });
-    pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [newUser['seat'], newUser['room_id'], newUser['id']], (err, res) => {
+    pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [userToSeat['seat'], userToSeat['room_id'], userToSeat['id']], (err, res) => {
       if (err) {
         console.log(err);
         return;
       }
     });
-    console.log(`User ${newUser['surname']} sits in room ${newUser['room_id']} in place ${newUser['seat']} :insert next:  avail_seat = ${currentRoom['available_seats']} has left`);
+    console.log(`User ${userToSeat['surname']} sits in room ${userToSeat['room_id']} in place ${userToSeat['seat']} :insert next:  avail_seat = ${availableRoom['available_seats']} has left`);
     return;
 
   }
 
   //XXX: Если люди из одной школы
   else {
-    newUser['seat'] = sortedUsers[sortedUsers.length - 1]['seat'] + 2
+    userToSeat['seat'] = sortedUsers[sortedUsers.length - 1]['seat'] + 2
     //XXX: Проверка на то, нужен ли переход на другое помещение
-    if (newUser['seat'] > currentRoom['number_of_seats']) {
-      currentRoomIndex += 1
-      if ((currentRoomIndex == placements.length)) {
-        console.log('All rooms occupied')
-        pool.query('DELETE FROM users WHERE id = $1', [newUser['id']], (err, res) => {
+    if (userToSeat['seat'] > availableRoom['number_of_seats']) {
+      availableRoomIndex += 1
+      if ((availableRoomIndex == placements.length)) {
+        // console.log('Cant set this person here')
+        pool.query('DELETE FROM users WHERE id = $1', [userToSeat['id']], (err, res) => {
           if (err) {
             console.log(err);
           } else {
-            console.log(`User with id ${newUser['id']} removed from database`);
+            console.log(`User with id ${userToSeat['id']} removed from database`);
           }
         });
+        handleUserSeatingError(userToSeat)
         return
       }
-      currentRoom = placements[currentRoomIndex]
-      const newFilteredUsers = users.filter(user => user['room_id'] === currentRoom['room_id']);
+      availableRoom = placements[availableRoomIndex]
+      const newFilteredUsers = users.filter(user => user['room_id'] === availableRoom['room_id']);
       const newSortedUsers = newFilteredUsers.sort((a, b) => a['seat'] - b['seat']);
 
       //XXX: Проверка на то, первый ли это объект после перемещения помещения
       if (newSortedUsers.length == 0) {
-        currentRoom['available_seats']--;
-        newUser['seat'] = 1;
-        newUser['room_id'] = currentRoom['room_id']
-        pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [currentRoom['available_seats'], currentRoom['room_id']], (err, res) => {
+        availableRoom['available_seats']--;
+        userToSeat['seat'] = 1;
+        userToSeat['room_id'] = availableRoom['room_id']
+        pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [availableRoom['available_seats'], availableRoom['room_id']], (err, res) => {
           if (err) {
             console.log(err);
             return;
           }
         });
-        pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [newUser['seat'], newUser['room_id'], newUser['id']], (err, res) => {
+        pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [userToSeat['seat'], userToSeat['room_id'], userToSeat['id']], (err, res) => {
           if (err) {
             console.log(err);
             return;
           }
         });
-        console.log(`User ${newUser['surname']} sits in room ${newUser['room_id']} in place ${newUser['seat']} :jump room start: avail_seat = ${currentRoom['available_seats']} has left`);
+        console.log(`User ${userToSeat['surname']} sits in room ${userToSeat['room_id']} in place ${userToSeat['seat']} :jump room start: avail_seat = ${availableRoom['available_seats']} has left`);
         return;
       }
       //XXX: Если после перемещения добавляемый объект не первый
       else {
-        newUser['seat'] = newSortedUsers[newSortedUsers.length - 1]['seat'] + 2
-        newUser['room_id'] = currentRoom['room_id']
-        currentRoom['available_seats']--;
-        pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [currentRoom['available_seats'], currentRoom['room_id']], (err, res) => {
+        userToSeat['seat'] = newSortedUsers[newSortedUsers.length - 1]['seat'] + 2
+        userToSeat['room_id'] = availableRoom['room_id']
+        availableRoom['available_seats']--;
+        pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [availableRoom['available_seats'], availableRoom['room_id']], (err, res) => {
           if (err) {
             console.log(err);
             return;
           }
         });
-        pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [newUser['seat'], newUser['room_id'], newUser['id']], (err, res) => {
+        pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [userToSeat['seat'], userToSeat['room_id'], userToSeat['id']], (err, res) => {
           if (err) {
             console.log(err);
             return;
           }
         });
-        console.log(`User ${newUser['surname']} sits in room ${newUser['room_id']} in place ${newUser['seat']} :jums room insert: avail_seat = ${currentRoom['available_seats']} has left`);
+        console.log(`User ${userToSeat['surname']} sits in room ${userToSeat['room_id']} in place ${userToSeat['seat']} :jums room insert: avail_seat = ${availableRoom['available_seats']} has left`);
         return;
       }
     }
     //XXX: Переход на другое помещение не нужен
     else {
-      newUser['room_id'] = currentRoom['room_id']
-      currentRoom['available_seats']--;
-      pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [currentRoom['available_seats'], currentRoom['room_id']], (err, res) => {
+      userToSeat['room_id'] = availableRoom['room_id']
+      availableRoom['available_seats']--;
+      pool.query('UPDATE placements SET available_seats = $1 WHERE room_id = $2', [availableRoom['available_seats'], availableRoom['room_id']], (err, res) => {
         if (err) {
           console.log(err);
           return;
         }
       });
-      pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [newUser['seat'], newUser['room_id'], newUser['id']], (err, res) => {
+      pool.query('UPDATE users SET seat = $1, room_id = $2 WHERE id = $3', [userToSeat['seat'], userToSeat['room_id'], userToSeat['id']], (err, res) => {
         if (err) {
           console.log(err);
           return;
         }
       });
-      console.log(`User ${newUser['surname']} sits in room ${newUser['room_id']} in place ${newUser['seat']} :jums insert: avail_seat = ${currentRoom['available_seats']} has left`);
+      console.log(`User ${userToSeat['surname']} sits in room ${userToSeat['room_id']} in place ${userToSeat['seat']} :jums insert: avail_seat = ${availableRoom['available_seats']} has left`);
       return;
     }
 
@@ -203,8 +209,9 @@ router.post('/store', async (req, res) => {
     const userData = await pool.query('SELECT * FROM users WHERE id = $1', [userId])
     const placements = await pool.query('SELECT * FROM placements ORDER BY room_id')
     const users = await pool.query('SELECT * FROM users')
-    const newUser = userData.rows[0]
-    setPerson(newUser, placements.rows, users.rows)
+    const userToSeat = userData.rows[0]
+    setPerson(userToSeat, placements.rows, users.rows)
+    // console.log(userToSeat)
 
     res.status(200).send({
       message: `Successfully added user`,
